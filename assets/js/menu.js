bb2cf926db077b7a5b5403e4e4c72ae3a6de58e2
Barber, Keenan <Keenan.Barber@biworldwide.com>
2017-06-19 11:50:21 -0500

    Centered Phaser window(?) and now it scales with the browser window.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -12,6 +12,17 @@ var Menu = {
 		// FOR SPRITES: this.add.sprite(0, 0, 'menu');
 
 		this.add.button(0, 0, 'menu', this.startGame, this);
+
+		game.scale.pageAlignVertically = true; 
+		game.scale.pageAlignHorizontally = true; 
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+		if (/Mobile|Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+		    console.log("This is running on a mobile device. ");
+		}
+		else {
+			console.log("This is not running on a mobile device. ");
+		}
 	}, 
 
 	startGame: function() {
